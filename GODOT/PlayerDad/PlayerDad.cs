a5f51eb84897484af8a393f51d5dafab17d8a303
Chloe Bai <145494305+chloe-bai@users.noreply.github.com>
2025-06-18 17:53:21 +0900

    Jump & crawl mechanics (#2)
    * feat: added jump and crawl mechanics
    
    * feat: jump and crawl mechanics
    
    ---------
    
    Co-authored-by: Chloe Bai <chloebai@Chloes-MacBook-Pro-2.local>

--- GODOT/PlayerDad/PlayerDad.cs
+++ GODOT/PlayerDad/PlayerDad.cs
@@ -6,6 +6,10 @@ public partial class PlayerDad : RigidBody3D
 	private float MouseSensitivity = 0.001f;
 	private float TwistInput = 0.0f;
 	private float PitchInput = 0.0f;
+	private float JumpVelocity = 4.5f;
+	private float Gravity = 9.8f;
+	private bool Crawling = false;
+	private Camera3D Camera;
 	private Node3D TwistPivot;
 	private Node3D PitchPivot;
 
@@ -16,6 +20,7 @@ public partial class PlayerDad : RigidBody3D
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 		TwistPivot = GetNode<Node3D>("TwistPivot");
 		PitchPivot = TwistPivot.GetNode<Node3D>("PitchPivot");
+		Camera = PitchPivot.GetNode<Camera3D>("Camera3D");
 	}
 
 	public override void _Process(double delta)
@@ -23,17 +28,33 @@ public partial class PlayerDad : RigidBody3D
 		Vector3 input = Vector3.Zero;
 		input.X = Input.GetAxis("move_left", "move_right");
 		input.Z = Input.GetAxis("move_forward", "move_backward"); 
-		//input.Y = Input.GetAxis("jump");
 
 		// Rotate input relative to player facing direction (TwistPivot)
 		Vector3 direction = TwistPivot.Basis * input;
 		ApplyCentralForce(direction * 1200.0f * (float)delta);
+		
+		// Jump, crawl mechanics
+		if (GetContactCount() > 0) {
+			if (Input.IsActionJustPressed("jump")) {
+				ApplyCentralImpulse(new Vector3(0, JumpVelocity, 0));
+			}
+			else if (Input.IsActionJustPressed("crawl")) {
+				Crawling = !Crawling;
+				if (Crawling) {
+					Camera.Position = new Vector3(0, 0.1f, 0);
+				}
+				else {
+					Camera.Position = new Vector3(0, 0.5f, 0);
+				}
+			}
+		}
 
 		if (Input.IsActionJustPressed("ui_cancel"))
 		{
 			Input.MouseMode = Input.MouseModeEnum.Visible;
 		}
-
+		
+		
 		// Apply horizontal rotation
 		TwistPivot.RotateY(TwistInput);
 
