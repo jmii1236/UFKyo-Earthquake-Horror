301d106e5b15278c1c9a9eada877fc98916cf378
jmii1236 <steiertskyler@gmail.com>
2025-06-21 15:44:48 +0900

    fire AI so far, not completed

--- GODOT/FireAI/FireAI.cs
+++ GODOT/FireAI/FireAI.cs
@@ -1,6 +1,6 @@
 using Godot;
 
-public partial class MomNPC : CharacterBody3D
+public partial class FireAI : CharacterBody3D
 {
 	private float speed = 2.0f;
 	private float accel = 10.0f;
@@ -26,7 +26,7 @@ public partial class MomNPC : CharacterBody3D
 
 		Vector3 direction = Vector3.Zero;
 
-		nav.TargetPosition = playerDad.GlobalPosition - Vector3.One; // Offset slightly to avoid collision;
+		nav.TargetPosition = playerDad.GlobalPosition; // Offset slightly to avoid collision;
 
 		// Check if we have a valid path
 		if (nav.IsNavigationFinished())
