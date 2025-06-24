2bf3f436be587aebe7d11aa544e0a8ee165104d1
Tristan Connolly <tristanconnolly1@gmail.com>
2025-06-24 23:15:43 +0900

    Items Class Outline + Pick up functionality (#10)
    * Added Items Class
    
    * Added Ability to Interact with Items and Pick them up
    
    * Fixed Merge issues
    
    * Made Level the main scene + Fixed Merge Conflicts

---
+++ GODOT/Items/Item.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class Item : Area3D
+{
+    public override void _Ready()
+    {
+
+    }
+
+    private void _On_Item_Use()
+    {
+
+    }
+}
--- GODOT/MomNPC/MomNPC.cs
+++ GODOT/MomNPC/MomNPC.cs
@@ -35,6 +35,8 @@ public partial class MomNPC : CharacterBody3D
 		direction = direction.Normalized();
 
 		Velocity = Velocity.Lerp(direction * speed, (float)(accel * delta));
+    
+		direction = Vector3.Zero;
 		
 		nav.TargetPosition = playerDad.GlobalPosition;
 		
