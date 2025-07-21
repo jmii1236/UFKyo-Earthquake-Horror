78654e1ab96775fca0caac68dbdf5c0a47adb4b4
jmii1236 <steiertskyler@gmail.com>
2025-07-21 20:11:17 +0900

    main menu + restart screens, WIP

---
+++ GODOT/GameUI/Restart/RestartQuitMenu.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class RestartQuitMenu : Node2D
+{
+  public void _on_restart_button_pressed()
+  {
+    // Logic to start the game
+    GD.Print("Start button pressed. Starting the game...");
+    // Here you would typically change the scene or start the game logic
+    GetTree().ChangeSceneToFile("res://level/Level.tscn");
+  }
+  public void _on_dead_quit_pressed()
+  {
+    // Logic to quit the game
+    GD.Print("Quit button pressed. Exiting the game...");
+    GetTree().Quit();    
+  }
+}
