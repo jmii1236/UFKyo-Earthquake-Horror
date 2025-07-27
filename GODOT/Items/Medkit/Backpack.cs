87a7d9cbdb7904ad18962e2e98addcc99b72bdc9
Chloe Bai <cbai71@yahoo.com>
2025-07-27 15:32:11 +0900

    emergency bag inv

---
+++ GODOT/Items/Medkit/Backpack.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Backpack : Item
+{
+	[Export] public InventoryData inventoryData;
+
+	[Signal] public delegate void InventoryToggleEventHandler(InventoryData ExternalInventory);
+	
+	public override void _Ready()
+	{
+		base._Ready(); // Call parent _Ready if Item has one
+		
+		// Add to the ExternalInventory group so it behaves like a chest
+		AddToGroup("ExternalInventory");
+		
+		// Remove from Item group so it won't be picked up
+		RemoveFromGroup("Item");
+	}
+	
+	public void PlayerInteract()
+	{
+		GD.Print("Player interacting with backpack");
+		EmitSignal(SignalName.InventoryToggle, inventoryData);
+	}
+}
