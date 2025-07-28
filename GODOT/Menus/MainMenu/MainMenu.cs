bdbc19a5ff6c22cfe33885d7b512a292d757f096
Chloe Bai <145494305+chloe-bai@users.noreply.github.com>
2025-07-28 23:05:22 +0900

    Collision fix (#53)

--- GODOT/Menus/MainMenu/MainMenu.cs
+++ GODOT/Menus/MainMenu/MainMenu.cs
@@ -3,30 +3,30 @@ using System;
 
 public partial class MainMenu : Control
 {
-    Button play_button, settings_button, quit_button;
-    public override void _Ready()
-    {
-        play_button = GetNode("%Play") as Button;
-        settings_button = GetNode("%Settings") as Button;
-        quit_button = GetNode("%Quit") as Button;
+	Button play_button, settings_button, quit_button;
+	public override void _Ready()
+	{
+		play_button = GetNode("%Play") as Button;
+		settings_button = GetNode("%Settings") as Button;
+		quit_button = GetNode("%Quit") as Button;
 
-        play_button.Pressed += on_play_button_pressed;
-        settings_button.Pressed += on_settings_button_pressed;
-        quit_button.Pressed += on_quit_button_pressed;
-    }
+		play_button.Pressed += on_play_button_pressed;
+		settings_button.Pressed += on_settings_button_pressed;
+		quit_button.Pressed += on_quit_button_pressed;
+	}
 
-    private void on_play_button_pressed()
-    {
-        GetTree().ChangeSceneToFile("res://level/level.tscn");
-    }
+	private void on_play_button_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://level/level.tscn");
+	}
 
-    private void on_settings_button_pressed()
-    {
-        GD.Print("Settings Pressed");
-    }
-    
-    private void on_quit_button_pressed()
-    {
-        GetTree().Quit();
-    }
+	private void on_settings_button_pressed()
+	{
+		GD.Print("Settings Pressed");
+	}
+	
+	private void on_quit_button_pressed()
+	{
+		GetTree().Quit();
+	}
 }
